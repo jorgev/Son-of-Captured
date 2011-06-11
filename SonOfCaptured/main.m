e6a31c2dbef50067618168bb3a7a0a13aac9e8db
Jorge Velázquez <jorge.velazquez@gmail.com>
2011-06-11 13:11:24 -0700

    working screenshot app, dimensions are hard-coded

--- SonOfCaptured/main.m
+++ SonOfCaptured/main.m
@@ -3,19 +3,23 @@
 //  SonOfCaptured
 //
 //  Created by Jorge Velázquez on 6/11/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Codeography. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
 int main (int argc, const char * argv[])
 {
-
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-	// insert code here...
-	NSLog(@"Hello, World!");
-
+	
+	CGRect rect = CGRectMake(300, 300, 400, 400);
+	CGImageRef screenShot = CGWindowListCreateImage(rect, kCGWindowListOptionOnScreenOnly, kCGNullWindowID, kCGWindowImageDefault);
+	NSBitmapImageRep *bitmapRep = [[NSBitmapImageRep alloc] initWithCGImage:screenShot];
+	NSData *data = [bitmapRep representationUsingType:NSPNGFileType properties: nil];
+	[data writeToFile:@"image.png" atomically:NO];
+	[bitmapRep release];
+	CGImageRelease(screenShot);
+	
 	[pool drain];
     return 0;
 }
