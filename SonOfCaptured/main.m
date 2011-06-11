6936ee89a1be37136623867fdb7d5c4855418dd6
Jorge Velázquez <jorge.velazquez@gmail.com>
2011-06-11 13:59:04 -0700

    now generating unique file name

--- SonOfCaptured/main.m
+++ SonOfCaptured/main.m
@@ -6,7 +6,21 @@
 //  Copyright 2011 Codeography. All rights reserved.
 //
 
+#include <stdlib.h>
 #import <Cocoa/Cocoa.h>
+
+static char alNum[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+NSString* CreateUniqueFilename(NSInteger numChars)
+{
+	char buf[32];
+	srand((unsigned int) time(NULL));
+	for (int i = 0; i < numChars; i++)
+		buf[i] = alNum[rand() % strlen(alNum)];
+	buf[numChars] = 0;
+	strcat(buf, ".png");
+	return [NSString stringWithCString:buf encoding:NSASCIIStringEncoding];
+}
 
 int main (int argc, const char * argv[])
 {
@@ -15,9 +29,10 @@ int main (int argc, const char * argv[])
 	CGRect rect = CGRectMake(300, 300, 400, 400);
 	CGImageRef screenShot = CGWindowListCreateImage(rect, kCGWindowListOptionOnScreenOnly, kCGNullWindowID, kCGWindowImageDefault);
 	NSBitmapImageRep *bitmapRep = [[NSBitmapImageRep alloc] initWithCGImage:screenShot];
-	NSData *data = [bitmapRep representationUsingType:NSPNGFileType properties: nil];
+	NSData *data = [bitmapRep representationUsingType:NSPNGFileType properties:nil];
 	NSString* tempFile = NSTemporaryDirectory();
-	tempFile = [tempFile stringByAppendingString:@"image.png"];
+	NSString* uniqueName = CreateUniqueFilename(5);
+	tempFile = [tempFile stringByAppendingString:uniqueName];
 	[data writeToFile:tempFile atomically:NO];
 	[bitmapRep release];
 	CGImageRelease(screenShot);
