45ad0179f54cd46d9176768bfe2b5820dc39dfba
Jorge Velázquez <jorge.velazquez@gmail.com>
2011-06-11 12:50:00 -0700

    Initial Commit

---
+++ SonOfCaptured/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SonOfCaptured
+//
+//  Created by Jorge Velázquez on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
